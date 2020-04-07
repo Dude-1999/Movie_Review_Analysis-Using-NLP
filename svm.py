24ae39499b6cae7bf029bd26bd55d02654166a08
Kausthub8 <47001638+Dude-1999@users.noreply.github.com>
2020-04-07 20:10:21 +0530

    Add files via upload
    Support Vector Algorithm(SVM) File. Nearly an accuracy of 89 percent is obtained for reviews

---
+++ svm.py
@@ -0,0 +1,20 @@
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.svm import LinearSVC
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import train_test_split
+
+ngram_vectorizer = CountVectorizer(binary=True, ngram_range=(1, 2))
+ngram_vectorizer.fit(reviews_train_clean)
+X = ngram_vectorizer.transform(reviews_train_clean)
+X_test = ngram_vectorizer.transform(reviews_test_clean)
+
+X_train, X_val, y_train, y_val = train_test_split(
+    X, target, train_size = 0.75
+)
+
+for c in [0.01, 0.05, 0.25, 0.5, 1]:
+    
+    svm = LinearSVC(C=c)
+    svm.fit(X_train, y_train)
+    print ("Accuracy for C=%s: %s" 
+           % (c, accuracy_score(y_val, svm.predict(X_val))))
